5a0dd6a407503e0f5d374739434c58028f9cf6e6
Serghei Garbalinschi <s_garbalinschi@flo.health>
2021-02-08 00:21:31 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+
+target 'SwinjectExample' do
+  use_frameworks!
+
+  pod "Swinject"
+  pod "RxSwift"
+end
---
+++ SwinjectExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwinjectExample
+//
+//  Created by Serghei Garbalinschi on 8.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
